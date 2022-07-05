8297ef4494bbf3d297c3185d0f9a5e8990973b6c
NAJIB-B <najibajibasu@gmail.com>
2022-07-05 11:57:27 +0100

    day 3 of react

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import CardList from "./component/card-list/card-list.component";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
       monsters: [],
+      searchfield: "",
     };
   }
   componentDidMount() {
@@ -19,16 +21,38 @@ class App extends Component {
               monsters: user,
             };
           },
-          () => {
-            console.log(this.state);
-          }
+          () => {}
         )
       );
   }
+  onSearchChange = (event) => {
+    const searchfield = event.target.value.toLowerCase();
+
+    this.setState(
+      () => {
+        return {
+          searchfield,
+        };
+      },
+      () => {}
+    );
+  };
   render() {
+    const { monsters, searchfield } = this.state;
+    const { onSearchChange } = this;
+    const filterdMonsters = monsters.filter((monster) => {
+      return monster.name.toLowerCase().includes(searchfield);
+    });
     return (
       <div className="App">
-        {this.state.monsters.map((monster) => {
+        <input
+          className="search-box"
+          type="search"
+          placeholder="search monster"
+          onChange={onSearchChange}
+        />
+        {/* <CardList /> */}
+        {filterdMonsters.map((monster) => {
           return <h1 key={monster.id}>{monster.name}</h1>;
         })}
       </div>
