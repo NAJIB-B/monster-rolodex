bf64a885b5ad487c029f1691f61d2813f13012d0
NAJIB-B <najibajibasu@gmail.com>
2022-07-05 09:43:04 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      monsters: [],
+    };
+  }
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((user) =>
+        this.setState(
+          () => {
+            return {
+              monsters: user,
+            };
+          },
+          () => {
+            console.log(this.state);
+          }
+        )
+      );
+  }
+  render() {
+    return (
+      <div className="App">
+        {this.state.monsters.map((monster) => {
+          return <h1 key={monster.id}>{monster.name}</h1>;
+        })}
+      </div>
+    );
+  }
+}
+
+export default App;
